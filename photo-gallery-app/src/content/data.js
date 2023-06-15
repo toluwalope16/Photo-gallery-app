7316d1ac75044f4f885ec7612dcbaa7222fc879a
tolu walope <TOLUODOLE16@GMAIL.COM>
2023-06-15 15:02:35 +0000

    first commit

---
+++ photo-gallery-app/src/content/data.js
@@ -0,0 +1,65 @@
+export const data = [
+    {
+      "name": "Photo 1",
+      "url": "/images/img1.jpg",
+      "description": "Photo 1 description",
+      "date": "2020-01-01",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 2",
+      "url": "/images/img2.jpg",
+      "description": "Photo 2 description",
+      "date": "2020-01-02",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 3",
+      "url": "/images/img33.jpg",
+      "description": "Photo 3 description",
+      "date": "2020-01-03",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 4",
+      "url": "/images/img44.jpg",
+      "description": "Photo 4 description",
+      "date": "2020-01-04",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 5",
+      "url": "/images/img55.jpg",
+      "description": "Photo 5 description",
+      "date": "2020-01-05",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 6",
+      "url": "/images/img66.jpg",
+      "description": "Photo 6 description",
+      "date": "2020-01-06",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 7",
+      "url": "/images/img77.jpg",
+      "description": "Photo 7 description",
+      "date": "2020-01-07",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 8",
+      "url": "/images/img8.jpg",
+      "description": "Photo 8 description",
+      "date": "2020-01-08",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 9",
+      "url": "/images/img9.jpg",
+      "description": "Photo 9 description",
+      "date": "2020-01-09",
+      "type": "image/jpg",
+    }
+  ]
